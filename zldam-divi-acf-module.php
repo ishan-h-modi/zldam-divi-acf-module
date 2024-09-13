5584e8cf599f1209701b02d1f673bf50a9be3eec
Ishan Modi <ishanm@zluck.in>
2024-09-13 16:50:15 +0530

    Start Developing Plugin

---
+++ zldam-divi-acf-module.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin Name: Divi ACF Module
+ * Description: Divi ACF Module seamlessly integrates Advanced Custom Fields with Divi Builder, allowing you to display dynamic ACF field data within your Divi layouts, enhancing customization and flexibility without any coding.
+ * Plugin URI: 
+ * Version: 1.0.0
+ * Author: Zluck Solutions
+ * Author URI: https://zluck.com
+ * License: GPLv3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: zldam-divi-acf-module
+ */
+
+// Declare the namespace for the Counter Block plugin
+namespace ZLDAM\DiviAcfModule;
+
+// Ensure the file is not accessed directly
+if (!defined('ABSPATH')) {
+    exit();
+}
